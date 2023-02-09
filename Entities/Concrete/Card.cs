6100a9a3692fe99265775e4029b4d466f3fb107e
feyzakaymakci <feyza.kaymakci@hotmail.com>
2023-02-09 11:01:36 +0300

    09.02.2023

---
+++ Entities/Concrete/Card.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Card:IEntity
+    {
+        public int CardId { get; set; }
+        public int CustomerId { get; set; }
+        public string OwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
+        public decimal? Debts { get; set; }
+    }
+}
